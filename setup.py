023e5eb7e54cfa9e18858416dffc10e86095441a
Dennis Iversen <dennis.iversen@gmail.com>
2023-12-11 15:19:17 +0100

    update setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='finviz-data',
+    version='0.1.0',
+    description='Simple script to get data from finviz.com',
+    author='Dennis Iversen',
+    author_email='dennis.iversen@gmail.com@com',
+    url='https://github.com/diversen/finviz-data',  # Optional
+    packages=find_packages(),
+    install_requires=required,
+    entry_points={
+        'console_scripts': [
+            # No scripts yet
+            # 'finviz-data=finviz_data.main:main',
+        ],
+    },
+)
